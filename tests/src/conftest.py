ca25ac176dc76a21ac9cdfec06beed051b7f95f1
jocosta96 <jeferson.o.costa@outlook.com>
2026-01-29 22:11:45 -0300

    revert conftest

--- tests/src/conftest.py
+++ tests/src/conftest.py
@@ -1,6 +1,4 @@
 import os
 import sys
 
-# Add project root to Python path
-project_root = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
-sys.path.insert(0, project_root)
+sys.path.append(os.getcwd())
