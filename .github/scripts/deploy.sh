d1525c34a5e67c3998218ccff93ff9de6aa690b2
jocosta96 <jeferson.o.costa@outlook.com>
2026-02-10 22:03:35 -0300

    moving to script

---
+++ .github/scripts/deploy.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+set -e
+
+SERVICE=${1:-video-processing}
+DPM_IMAGE=$2
+NLB_TARGET_GROUP_ARN=$3
+DEFAULT_REGION=$4
+
+sed -i 's/client.authentication.k8s.io\/v1alpha1/client.authentication.k8s.io\/v1beta1/g' ~/.kube/config
+
+TEMP_DIR=$(mktemp -d)
+cd $TEMP_DIR
+
+for manifest in cfm_database.yaml sec_app.yaml svc_app.yaml dpm_app.yaml hpa_app.yaml nlb_tgb.yaml; do
+  echo "Deploying $manifest..."
+  sed \
+    -e "s|\${dpm_name}|dpm-${SERVICE}|g" \
+    -e "s|\${cfm_name}|cfm-database-${SERVICE}|g" \
+    -e "s|\${sec_name}|sec-app-${SERVICE}|g" \
+    -e "s|\${app_sec_name}|sec-app-${SERVICE}|g" \
+    -e "s|\${dpm_image}|${DPM_IMAGE}|g" \
+    -e "s|\${load_balancer_name}|svc-app-lb-${SERVICE}|g" \
+    -e "s|\${target_group_arn}|${NLB_TARGET_GROUP_ARN}|g" \
+    -e "s|\${tgb_name}|tgb-${SERVICE}|g" \
+    -e "s|\${hpa_name}|hpa-app-${SERVICE}|g" \
+    -e "s|\${region}|${DEFAULT_REGION}|g" \
+    "/home/ec2-user/manifests/$manifest" | kubectl apply --validate=false -f -
+done
+
+rm -rf $TEMP_DIR
